8c7d60728379d9924d283693ab98d2b232771079
David Scotson <david.scotson@avadolearning.com>
2018-02-23 23:27:20 +0000

    minor tweak

--- moodle.sh
+++ moodle.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 bundle exec jekyll build
-cd _gh_pages;
-find . -name '*.html' -exec sh -c 'mv "$0" "${0%.html}.php"' {} \;
-cd ..
-cp -r _gh_pages/docs ../Moodle/admin/tool/themetester/.
+find _gh_pages/docs/ -name '*.html' -exec sh -c 'mv "$0" "${0%.html}.php"' {} \;
+cp -r _gh_pages/docs ../moodle-tool_themetester/.
+cp -r assets ../moodle-tool_themetester/.
