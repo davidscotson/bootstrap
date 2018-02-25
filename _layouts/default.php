489483b46925920c4a5cd406c7762c3101b2181d
David Scotson <david.scotson@avadolearning.com>
2018-02-25 16:35:40 +0000

    more tweaks

---
+++ _layouts/default.php
@@ -0,0 +1,14 @@
+    {% include header.html %}
+
+    {% if page.layout == "simple" %}
+      {{ content }}
+    {% else %}
+      <main id="content" role="main">
+        {{ content }}
+      </main>
+    {% endif %}
+
+    {% include footer.html %}
+    {% include scripts.html %}
+  </body>
+</html>
